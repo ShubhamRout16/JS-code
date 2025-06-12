d71f29ee5af32ed77d3f56673e5294c8c8abbdae
ShubhamRout16 <routshubham2028@gmail.com>
2025-06-12 23:38:47 +0530

    new changes

---
+++ chai_aur_code/dom/dom.js
@@ -0,0 +1,19 @@
+// DOM
+// how to manipulate text on webpage
+// document.getElementById("idofelelement").innerHTML = "Shubham ROUT"
+
+// to get the name of the id of the element
+// console.log(document.getElementById('header').id);
+
+// to get the name of te class
+// console.log(document.getElementById('header').className);
+
+// getAttribute
+// document.getElementById('header').getAttribute('id'); 
+// takes key as input and returns value as the output
+// document.getElementById('header').getAttribute('class');
+
+// setAttribute
+// document.getElementById('header').setAttribute('class','new_class heading')
+// setAttribute me jis chiz ko add krna ha uska key sath me add honi vali value ko add krna ha , imp value gets overwrite so also put the old value 
+
