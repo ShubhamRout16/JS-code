d0d1642967d2dc8529ee8ef9727649e45acab9b0
ShubhamRout16 <routshubham2028@gmail.com>
2025-06-24 14:49:51 +0530

    js : hoisting in js

---
+++ chai_aur_code/namaste-JS/hoisting.js
@@ -0,0 +1,16 @@
+getName();
+getName2();
+console.log(x);
+
+var x = 9 
+
+// used as arrow function
+var getName2 = () => {
+  console.log("hello twice");
+}
+
+// function saved in variable
+var getName = function getName(){
+  console.log("hello i am shubham rout");
+}
+
