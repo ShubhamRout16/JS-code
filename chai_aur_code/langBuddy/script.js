39f8188210a2de803e0e359f831d1ddb4547eb34
ShubhamRout16 <routshubham2028@gmail.com>
2025-06-25 10:32:25 +0530

    langbuddy : localStorage & filter for learned & difficult words added

--- chai_aur_code/langBuddy/script.js
+++ chai_aur_code/langBuddy/script.js
@@ -1,10 +1,10 @@
 // we have created a array of objects with word and meaning
-const vocabulary = [
+let vocabulary = [
   {word: "hello" , meaning: "greeting" , status: "new"},
   {word: "intend" , meaning: "have in mind as a purpose" , status: "new"},
   {word: "concern" , meaning: "something that interests you because it is important" , status: "new"},
   {word: "vain" , meaning: "unproductive of success" , status: "new"},
-  {word: " entertain" , meaning: "provide amusement for" , status: "new"},
+  {word: "entertain" , meaning: "provide amusement for" , status: "new"},
   {word: "knight" , meaning: "a person of noble birth trained to arms and chivalry" , status: "new"},
   {word: "inspire" , meaning: "serve as the inciting cause of" , status: "new"},
   {word: "skill" , meaning: "an ability that has been acquired by training" , status: "new"},
@@ -34,7 +34,7 @@ const difficultButton = document.getElementById('difficultButton')
 
 function changeStatus(text){
   // check if curreIndex exceeds the array
-  if((currentIndex + 1) > vocabulary.length){
+  if(currentIndex >= vocabulary.length){
     container.innerHTML = `
     <p><strong>No more Words</strong></p>
     `
@@ -43,6 +43,8 @@ function changeStatus(text){
     
     console.log("came");
     vocabulary[currentIndex].status = text;
+    // calling vocab saving function after changing the text content so it stores it to the localStorage
+    saveVocabulary();
     console.log(vocabulary[currentIndex].status);
     currentIndex++
     showWord(currentIndex);
@@ -50,7 +52,48 @@ function changeStatus(text){
   
 }
 
+// using localStorage so that the data persists
+function saveVocabulary(){
+  //why like this? -> we are directly storing the data on which changes is made , because when page reloads 
+  // vocabulary resets its status to its initial value so the changes made wont be seen
+  localStorage.setItem('savedVocab', JSON.stringify(vocabulary))
+}
+
+// next feature we will add a filter so that users can see learned words and difficult words 
+function showFilteredWords(recievedBtn){
+  container.innerHTML = ''
+  if(recievedBtn === 'viewLearned'){
+    const filteredArrayLearned = vocabulary.filter(vocabulary => vocabulary.status === 'learned')
+    for(let i = 0; i < filteredArrayLearned.length ; i++){
+      container.innerHTML += `
+      <p><strong>${filteredArrayLearned[i].word}</strong></p>
+      <p><strong>${filteredArrayLearned[i].meaning}</strong></p>
+      `
+    }
+    console.log(filteredArrayLearned);
+  }
+  if(recievedBtn === 'viewDifficult'){
+    const filteredArrayDifficult = vocabulary.filter(vocabulary => vocabulary.status === 'Difficult')
+    for(let i = 0; i < filteredArrayDifficult.length ; i++){
+      container.innerHTML += `
+      <p><strong>${filteredArrayDifficult[i].word}</strong></p>
+      <p><strong>${filteredArrayDifficult[i].meaning}</strong></p>
+      `
+    }
+    console.log(filteredArrayDifficult);
+  }
+} 
+
+
+
 // show the first word initially when page loads
 window.onload = () => {
+  // loading vocabulary with changes made
+  const savedVocab = JSON.parse(localStorage.getItem('savedVocab')) || []
+  if(savedVocab.length > 0){
+    vocabulary = savedVocab;
+  }
+  // console.log(vocabulary); //checked its loading previously saved data
+  
   showWord(currentIndex);
 }
