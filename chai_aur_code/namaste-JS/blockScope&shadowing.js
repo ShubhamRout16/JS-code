2f134241d6f6f52eb3151d50e9c6bd843d9297cb
ShubhamRout16 <routshubham2028@gmail.com>
2025-06-26 12:38:38 +0530

    js : block scope and shadowing

---
+++ chai_aur_code/namaste-JS/blockScope&shadowing.js
@@ -0,0 +1,33 @@
+// {
+//   // compound statement
+//   var a = 100
+//   console.log(a);
+// }
+
+// if(true) {needs single code}
+// if(true){
+//   // compound statement
+//   var a = 100
+//   console.log(a);
+// }
+
+
+// {
+//   var a = 10
+//   let b = 20
+//   const c = 30
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+var a = 100
+{
+  var a = 10
+  console.log(a); // 10
+}
+console.log(a); // 10
+
