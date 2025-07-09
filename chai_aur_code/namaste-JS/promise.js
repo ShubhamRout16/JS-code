9a9285bc5b4f177eb2699c3935e3f06e754ef14d
ShubhamRout16 <routshubham2028@gmail.com>
2025-07-09 14:19:10 +0530

    promise creation and resolve reject

--- chai_aur_code/namaste-JS/promise.js
+++ chai_aur_code/namaste-JS/promise.js
@@ -3,5 +3,26 @@ const cart = ["shoes","kurtas","pajamas"]
 const promise = createOrder(cart) // will return a promise object with orderID on completion
 // consuming a promise
 promise.then(function(orderId){
-  proceedToPayment(orderId)
+  console.log(orderId);
+  // proceedToPayment(orderId)
 })
+
+// creating a promise
+function createOrder(cart){
+  
+  const pr = new Promise(function(resolve,reject){
+    if(!validateCart(cart)){
+      const err = new Error('cart is not valid')
+      reject(err)
+    }
+    const orderId = '12345'
+    if(orderId){
+      resolve(orderId)
+    }
+  })
+
+  return pr;
+}
+function validateCart(cart){
+  return true
+}
